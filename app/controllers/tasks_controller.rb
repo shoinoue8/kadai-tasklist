ccf0513d2503127e5555a9283ce0c39dd461865a
inosho827 <ynw.insh@gmail.com>
2020-01-04 05:17:05 +0000

    login function

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,28 +1,32 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  
   def index
-    @tasks = Task.all
+    @tasks = current_user.tasks.order(id: :desc).page(params[:page])
   end
   
   def show
   end
-  
+
   def new
     @task = Task.new
   end
   
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     if @task.save
-      flash[:success] = 'タスクが正常に登録されました'
-      redirect_to @task
+      flash[:success] = 'タスクが登録されました。'
+      redirect_to root_path
     else
-      flash.now[:danger] = 'タスクが登録されませんでした'
-      render :new
+      @tasks = current_user.microposts.order(id: :desc).page(params[:page])
+      flash.now[:danger] =　'タスクの登録に失敗しました。'
+      render 'tasks'
     end
   end
   
   def edit
+    @task = Task.find(params[:id])
   end
   
   def update
